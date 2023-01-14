673d38763423a9b50edd1ea988d0c5d3bf5b195b
BHARATHI <88837161+BHARATHI-GIT-HUB@users.noreply.github.com>
2023-01-14 16:10:02 +0530

    mobile view commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ function App() {
   const { settings } = useContext(Context);
   return (
     <div>
-      <div className="grid grid-rows-4  min-w-screen min-h-screen overflow-hidden">
-        <Header />
-        <Main />
+      <div className="md:block hidden">
+        <div className="grid grid-rows-4  min-w-screen min-h-screen overflow-hidden">
+          <Header />
+          <Main />
+        </div>
+        <AlgoContent AlgoType={settings.algoType} />
       </div>
-      <AlgoContent AlgoType={settings.algoType} />
+      <div className="md:hidden bg-black min-h-screen text-white flex justify-center items-center capitalize font-inter font-noraml font-3xl">
+        project isn't designed for moblie view!! please switch to destop view
+      </div>
     </div>
   );
 }
